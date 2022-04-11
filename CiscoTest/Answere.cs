c241f7e1353f4c60dd490d59c712d0cca9621184
Raal <ivan.pyzhyanov.90@mail.ru>
2022-04-11 17:18:46 +0300

    init proj

---
+++ CiscoTest/Answere.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace CiscoTest
+{
+    [Serializable]
+    public class Answere : ObservableObject
+    {
+        /// <summary>
+        /// Текст вопроса
+        /// </summary>
+        public string AnswereText { get; set; }
+
+        /// <summary>
+        /// Тег ответа
+        /// </summary>
+        public string Tag { get; set; }
+
+        /// <summary>
+        /// Флаг выбора
+        /// </summary>
+        private bool isChecked;
+        public bool IsChecked { get { return isChecked; } 
+            set { 
+                isChecked = value; 
+                OnPropsChanged("IsChecked");
+                Checked?.Invoke(this, null);
+            } 
+        }
+
+        [JsonIgnore]
+        public EventHandler Checked { get; set; }
+    }
+}
---
+++ CiscoTest/ObservableObject.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace CiscoTest
+{
+    public class ObservableObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropsChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
